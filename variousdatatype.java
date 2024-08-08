460d6c2ac9f6652fe30b2f3f1ad595254645f2e7
Shobhit1805 <23bit165@ietdavv.edu.in>
2024-08-08 15:42:26 +0530

    Add files via upload

---
+++ variousdatatype.java
@@ -0,0 +1,24 @@
+public class variousdatatype{
+
+public static void main(String[] args) {
+    int a  =  246387;
+    long l =34523452;
+    byte b = 127;
+    char c = 'c';
+    short s = 32767; 
+    double d = 232.2323;
+    float f =  1.1234568f ;
+
+    System.out.println(a);
+    System.out.println(l);
+    System.out.println(b);
+    System.out.println(c);
+    System.out.println(s);
+    System.out.println(d);
+    System.out.println(f);
+
+
+        
+    }
+
+}
