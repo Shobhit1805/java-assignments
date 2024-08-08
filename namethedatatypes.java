34df155ad3134c2dc4907458edd20292c9ac4aa5
Shobhit1805 <23bit165@ietdavv.edu.in>
2024-08-08 15:43:48 +0530

    Add files via upload

---
+++ namethedatatypes.java
@@ -0,0 +1,9 @@
+//import java.util.*;
+public class datatypes{
+    public static void main(String args[]){
+        Object a = 5;
+        
+
+        System.out.println(a.getClass().getSimpleName());
+    }
+}
