57ef3cd2060027039755cce7239036bfc6d1793d
Shobhit1805 <23bit165@ietdavv.edu.in>
2024-11-16 22:21:49 +0530

    Add files via upload

---
+++ Simple.java
@@ -0,0 +1,12 @@
+package example;
+
+public class Simple {
+    
+    public void show(){
+        System.out.println("welcome to package");
+    }
+    public static void main(String[] args) {
+        Simple s1= new Simple();
+        s1.show();
+    }
+}
